7aed6cf9fbf1b7bf134c75ce8fe4ea952ec21f70
SH01083ML <sh01083ml@SH01083MLs-MBP.lan>
2024-07-12 18:19:50 +0800

    feat(app): validate form fields on the server

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -1,23 +1,45 @@
 'use client'
 
+import { useActionState } from 'react'
 import { signup } from '@/app/actions/auth'
+import { useFormStatus } from 'react-dom'
 
 export function SignupForm() {
+  const [state, action] = useActionState(signup, undefined)
+  const { pending } = useFormStatus()
+
   return (
-    <form action={signup}>
+    <form action={action}>
       <div>
         <label htmlFor="name">Name</label>
         <input id="name" name="name" placeholder="Name" />
       </div>
+      {state?.errors?.name && <p>{state.errors.name}</p>}
+
       <div>
         <label htmlFor="email">Email</label>
         <input id="email" name="email" type="email" placeholder="Email" />
       </div>
+      {state?.errors?.email && <p>{state.errors.email}</p>}
+
       <div>
         <label htmlFor="password">Password</label>
         <input id="password" name="password" type="password" />
       </div>
-      <button type="submit">Sign Up</button>
+      {state?.errors?.email && (
+        <div>
+          <p>Password must:</p>
+          <ul>
+            {state.errors.password!.map((error) => (
+              <li key={error}>- {error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <button aria-disabled={pending} type="submit">
+        {pending ? 'Submitting...' : 'Sign up'}
+      </button>
     </form>
   )
 }
