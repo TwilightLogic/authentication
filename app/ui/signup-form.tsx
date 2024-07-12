35b6d6c56b1bd0f79d39224e113748a14c8fe840
SH01083ML <sh01083ml@SH01083MLs-MacBook-Pro.local>
2024-07-12 23:31:18 +0800

    feat(app): update ui

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -9,36 +9,75 @@ export function SignupForm() {
   const { pending } = useFormStatus()
 
   return (
-    <form action={action}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" placeholder="Name" />
-      </div>
-      {state?.errors?.name && <p>{state.errors.name}</p>}
-
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" placeholder="Email" />
-      </div>
-      {state?.errors?.email && <p>{state.errors.email}</p>}
-
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      {state?.errors?.password && (
+    <div className="bg-zinc-900 p-8 rounded-lg max-w-md w-full shadow-2xl">
+      <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+      <form action={action} className="space-y-4">
         <div>
-          <p>Password must:</p>
-          <ul>
-            {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-400"
+          >
+            Name
+          </label>
+
+          <input
+            id="name"
+            name="name"
+            placeholder="Name"
+            className="mt-1 block w-full p-2 border border-gray-500 rounded-md shadow-sm focus:outline-none bg-black"
+          />
         </div>
-      )}
-      <button aria-disabled={pending} type="submit">
-        {pending ? 'Submitting...' : 'Sign up'}
-      </button>
-    </form>
+        {state?.errors?.name && <p>{state.errors.name}</p>}
+
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-400"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="mt-1 block w-full p-2 border border-gray-500 rounded-md shadow-sm focus:outline-none bg-black"
+          />
+        </div>
+        {state?.errors?.email && <p>{state.errors.email}</p>}
+
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-400"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="password"
+            className="mt-1 block w-full p-2 border border-gray-500 rounded-md shadow-sm focus:outline-none bg-black"
+          />
+        </div>
+        {state?.errors?.password && (
+          <div>
+            <p>Password must:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>- {error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <button
+          aria-disabled={pending}
+          type="submit"
+          className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          {pending ? 'Submitting...' : 'Sign up'}
+        </button>
+      </form>
+    </div>
   )
 }
